5b546b507b1af3fe510871d33e6865081e21a112
24019287 <163423208+24019287@users.noreply.github.com>
2024-04-15 15:55:44 +0200

    Add files via upload

---
+++ sub.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class sub{
+public static void main(String[] args){
+
+    Scanner scann = new Scanner(System.in);
+     int a;
+     int b;
+     int c;
+     
+     System.out.println("Enter your first number");
+     a = scann.nextInt();
+     
+        System.out.println("Enter your second number");
+     b = scann.nextInt();
+     c = a - b;
+     
+        System.out.println("Answer is "+ c);
+    
+    
+}
+}
